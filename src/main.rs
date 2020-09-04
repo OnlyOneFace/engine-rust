9dd7599cb7c118bea4d571184dbe03737e72a563
OnlyOneFace <13522570308@139.com>
2020-09-05 00:17:39 +0800

    add

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 mod logger;
 
 use log::info;
+use actix_web::{get, web, App, HttpServer, Responder};
 
-fn main() {
+#[get("/{id}/{name}")]
+async fn index(web::Path((id, name)): web::Path<(u32, String)>) -> impl Responder {
+    format!("Hello {}! id:{}", name, id)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
     logger::init();
     info!("run ...");
+    HttpServer::new(|| App::new().service(index))
+        .bind("127.0.0.1:8083")?
+        .run()
+        .await
 }
