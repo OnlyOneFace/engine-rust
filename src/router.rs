83c611842aa167f932e9ff0ebd6ef3dd3d0c879a
OnlyOneFace <13522570308@139.com>
2020-09-06 01:00:12 +0800

    add

--- src/router.rs
+++ src/router.rs
@@ -1,16 +1,19 @@
 use actix_web::{post, web, Responder};
 use std::collections::HashMap;
+use serde::Serialize;
 
-enum JsonValue<T> {
-    tv(T),
+#[derive(Serialize)]
+enum JsonValue {
+    String(String),
+    U32(u32),
     AnotherHashMap(HashMap<String, String>),
 }
 
 #[post("/{id}/{name}")]
 pub async fn index(web::Path((id, name)): web::Path<(u32, String)>, query: web::Query<HashMap<String, String>>, body: web::Json<HashMap<String, String>>) -> impl Responder {
-    let mut temp_map: HashMap<&str, JsonValue<T>> = HashMap::new();
-    temp_map.insert("hello", JsonValue::tv(name));
-    temp_map.insert("id", JsonValue::tv(id));
+    let mut temp_map: HashMap<&str, JsonValue> = HashMap::new();
+    temp_map.insert("hello", JsonValue::Tv(name));
+    temp_map.insert("id", JsonValue::U32(id));
     temp_map.insert("query", JsonValue::AnotherHashMap(query.into_inner()));
     temp_map.insert("body", JsonValue::AnotherHashMap(body.into_inner()));
     web::Json(temp_map)
