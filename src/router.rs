5b679efd96ab08372bf262a178bf98e03b4262e1
OnlyOneFace <13522570308@139.com>
2020-09-06 02:29:36 +0800

    add

--- src/router.rs
+++ src/router.rs
@@ -1,20 +1,30 @@
-use actix_web::{post, web, Responder};
+use actix_web::{post, web, dev, Responder, HttpMessage};
 use std::collections::HashMap;
+use std::any::Any;
 use serde::Serialize;
 
 #[derive(Serialize)]
-enum JsonValue {
-    String(String),
-    U32(u32),
-    AnotherHashMap(HashMap<String, String>),
+struct AnyThing {
+    name: Box<dyn Any>,
+}
+
+impl AnyThing {
+    fn new(a: Box<dyn Any>) -> AnyThing {
+        AnyThing {
+            name: a,
+        }
+    }
 }
 
 #[post("/{id}/{name}")]
-pub async fn index(web::Path((id, name)): web::Path<(u32, String)>, query: web::Query<HashMap<String, String>>, body: web::Json<HashMap<String, String>>) -> impl Responder {
-    let mut temp_map: HashMap<&str, JsonValue> = HashMap::new();
-    temp_map.insert("hello", JsonValue::Tv(name));
-    temp_map.insert("id", JsonValue::U32(id));
-    temp_map.insert("query", JsonValue::AnotherHashMap(query.into_inner()));
-    temp_map.insert("body", JsonValue::AnotherHashMap(body.into_inner()));
+pub async fn index(web::Path((id, name)): web::Path<(u32, String)>,
+                   query: web::Query<HashMap<String, String>>,
+                   body: web::Json<HashMap<String, String>>) -> impl Responder {
+    let mut temp_map: HashMap<&str, AnyThing> = HashMap::new();
+    temp_map.insert("hello", AnyThing::new(Box::new(name)));
+    temp_map.insert("id", AnyThing::new(Box::new(id)));
+    temp_map.insert("query", AnyThing::new(Box::new(query.into_inner())));
+    //temp_map.insert("header");
+    temp_map.insert("body", AnyThing::new(Box::new(body.into_inner())));
     web::Json(temp_map)
 }
