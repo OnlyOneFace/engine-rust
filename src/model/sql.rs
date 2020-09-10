a2a30b4c20444468d7f3a3ad458b08dd9bbf7ab6
OnlyOneFace <13522570308@139.com>
2020-09-11 00:04:25 +0800

    add

--- src/model/sql.rs
+++ src/model/sql.rs
@@ -1,41 +1,20 @@
-use sqlx::{MySqlPool, MySqlConnection, Pool};
-use std::time;
-use log::{info, error};
-use std::sync::{Arc, Mutex};
+use mysql_async::Pool;
 
-static mut CONN_POOL: Option<Arc<Mutex<Pool<MySqlConnection>>>> = None;
+static mut CONN_POOL: Pool = init_sql();
 
-pub fn get_sql() -> Arc<Mutex<Pool<MySqlConnection>>> {
-    unsafe {// Rust中使用可变静态变量都是unsafe的
-        CONN_POOL.get_or_insert_with(|| {
-            // 初始化单例对象的代码
-            Arc::new(Mutex::new(init_sql()))
-        }).clone()
-    }
+pub fn get_sql() -> Pool {
+    unsafe { &*CONN_POOL }
 }
 
-fn init_sql() -> Pool<MySqlConnection> {
-    let sql_url = format!("%{}:%{}@tcp(%{}:%{})/%{}?charset=%v&parseTime=True&loc=Local",
+fn init_sql() -> Pool {
+    let sql_url = format!("mysql://{}:{}@{}:{}/{}?pool_min={}&pool_max={}&conn_ttl=10",
                           "root",
                           "123456",
                           "192.168.31.62",
                           3306,
-                          "identity_information"
+                          "identity_information",
+                          10,
+                          100,
     );
-    info! {"{}", sql_url};
-    let pool = MySqlPool::builder().
-        max_size(100).
-        min_size(10).
-        connect_timeout(time::Duration::from_secs(10)).
-        max_lifetime(time::Duration::from_secs(1800)).
-        idle_timeout(time::Duration::from_secs(600)).
-        build(sql_url.as_str()).await;
-    match pool {
-        Ok(conn) => conn,
-        Err(e) => {
-            let msg = format!("mysql connect failed.Error:{:?}", e);
-            error! {"{}", msg};
-            panic!("{}", msg)
-        }
-    }
+    Pool::new(sql_url)
 }
